14804a35c47002c7d11c853c3978d23262089d70
Dandy Candra <dandycandrapratama@gmail.com>
2024-07-25 10:34:31 +0700

    add new media

---
+++ public/SVG/index.ts
@@ -0,0 +1,5 @@
+export { default as EyeSVG } from "./Eye";
+export { default as ThumbUpSVG } from "./ThumbUP";
+export { default as ThumbDownSVG } from "./ThumbDown";
+export { default as ChevronLeftSVG } from "./ChevronLeft";
+export { default as ChevronRightSVG } from "./ChevronRight";
