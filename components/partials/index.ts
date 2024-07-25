a6ace53378a1b7d49b54b7d80f93b782e1e16649
Dandy Candra <dandycandrapratama@gmail.com>
2024-07-25 10:35:24 +0700

    add export member to partials components

---
+++ components/partials/index.ts
@@ -0,0 +1,4 @@
+export { default as CardPost } from "./CardPost";
+export { default as Pagination } from "./Pagination";
+export { default as OverlayLoading } from "./OverlayLoading";
+export { default as ErrorView } from "./ErrorView";
