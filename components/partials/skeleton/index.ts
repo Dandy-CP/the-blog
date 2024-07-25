b203e88ff4114c2d03d10dcb9efb888cc9273c48
Dandy Candra <dandycandrapratama@gmail.com>
2024-07-25 10:35:35 +0700

    add export member to partial skeleton

---
+++ components/partials/skeleton/index.ts
@@ -0,0 +1,3 @@
+export { default as CardHighlightSkeleton } from "./CardHighlightSkeleton";
+export { default as CardPostSkeleton } from "./CardPostSkeleton";
+export { default as CommentSkeleton } from "./CommentSkeleton";
